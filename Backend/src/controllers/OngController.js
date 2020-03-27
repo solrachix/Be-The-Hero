add0ad1f10a303c39e45035cc46e4fe55c849bd9
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2020-03-27 05:57:12 -0300

    fixing bug of generating repeated IDS

--- Backend/src/controllers/OngController.js
+++ Backend/src/controllers/OngController.js
@@ -2,6 +2,26 @@ const crypto = require('crypto');
 
 const connection = require('../database/connection');
 
+// Gerar novo ID
+function genareteId() {
+  const id = crypto.randomBytes(4).toString('HEX')
+  return checkId(id)
+}
+
+async function checkId(id) {
+  // Vai retornar verdadeiro se o id existir
+  const ongWithIdExist = await connection('ongs')
+    .select('*')
+    .where({ 'id': id })
+    .first();
+
+  while (ongWithIdExist) {
+    id = genareteId();
+  }
+
+  return id
+}
+
 module.exports = {
   async index (request, responce) {
     const ongs = await connection('ongs').select('*');
@@ -11,7 +31,7 @@ module.exports = {
   async create(request, responce) {
     const { name, email, whatsapp, city, uf } = request.body;
   
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = await genareteId()
   
     await connection('ongs').insert({
       id,
